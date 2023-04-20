224ab2453921c8937f079a567bf9ce75f4aeedc3
a22alfna <a22alfna@student.his.se>
2023-04-20 11:00:44 +0200

    Jag lade till möjligheten att skicka data från första sidan till den andra med hjälp av intent bundle. Jag gjorde även så att det är det man skriver in i textview på första sidan som följer med till andra sidan och inte bara en förbestämd text.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendButton = findViewById(R.id.sendButton);
+
+        sendButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+                public void onClick(View View){
+                    Intent intent = new Intent(MainActivity.this, taEmotData.class);
+                    EditText nameEdit = findViewById(R.id.editTextName);
+                    EditText ageEdit = findViewById(R.id.editTextAge);
+
+                    String name = nameEdit.getText().toString();
+                    String age = ageEdit.getText().toString();
+
+                    intent.putExtra("name", name); // Optional
+                    intent.putExtra("number", age); // Optional
+                    startActivity(intent);
+            }
+        });
     }
 }
